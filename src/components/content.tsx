e810f2bf020ff08590d3c99b5a817f067433c142
M7kra <luiswbarbosa@gmail.com>
2024-03-31 22:55:17 +0100

    Added download link for v1.0.0

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -6,7 +6,7 @@ export default function Content()  {
             <div>
                 <h1>Vortex</h1>
                 <p>Introducing Vortex, a simple and open-source Android podcast player developed by <a href='https://bogosorter.github.io'>bogosorter</a>. This is a test version - a stable version will be released soon. Vortex emphasizes a clean and simple style. It has all the basic features of a podcast player except for automatic downloads. All feedback is appreciated: you may open an issue on GitHub or send me an email: <code>luiswbarbosa@gmail.com</code></p>
-                <a href='./'><button className='experimental'>Experimental version</button></a>
+                <a href='https://github.com/bogosorter/vortex/releases/download/v1.0.0/vortex.apk'><button className='experimental'>Download</button></a>
             </div>
         </div>
     )
