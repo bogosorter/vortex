4465378bafa968f1a15c29db02e3fe0bbc8504a8
M7kra <luiswbarbosa@gmail.com>
2024-04-01 14:15:00 +0100

    Update

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,11 +1,16 @@
 import './content.css';
 
 export default function Content()  {
+    const small = window.innerWidth < 800;
+    const text = small?
+        <p>Introducing Vortex, an Android podcast player developed by <a href='https://bogosorter.github.io'>bogosorter</a>. Reach me out at <code>luiswbarbosa@gmail.com</code></p> :
+        <p>Introducing Vortex, a simple and open-source Android podcast player developed by <a href='https://bogosorter.github.io'>bogosorter</a>. This is a test version - a stable version will be released soon. Vortex emphasizes a clean and simple style. It has all the basic features of a podcast player except for automatic downloads. All feedback is appreciated: you may open an issue on GitHub or send me an email: <code>luiswbarbosa@gmail.com</code></p>
+
     return (
         <div className='content'>
             <div>
                 <h1>Vortex</h1>
-                <p>Introducing Vortex, a simple and open-source Android podcast player developed by <a href='https://bogosorter.github.io'>bogosorter</a>. This is a test version - a stable version will be released soon. Vortex emphasizes a clean and simple style. It has all the basic features of a podcast player except for automatic downloads. All feedback is appreciated: you may open an issue on GitHub or send me an email: <code>luiswbarbosa@gmail.com</code></p>
+                {text}
                 <a href='https://github.com/bogosorter/vortex/releases/download/v1.0.0/vortex.apk'><button className='experimental'>Download</button></a>
             </div>
         </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,10 @@
 import logo from '../images/semi-transparent.png';
+import GitHub from './github';
 import './header.css';
 
 export default function Header() {
+    const small = window.innerWidth < 800;
+
     return (
         <div className='header'>
             <a href='./'>
@@ -10,8 +13,16 @@ export default function Header() {
             <a href='./'>
                 <div className='header-link bold'>Vortex</div>
             </a>
-            <a href='https://bogosorter.github.io'>
-                <div className='header-link hide-small'>bogosorter</div>
+            {!small &&
+                <a href='https://bogosorter.github.io'>
+                    <div className='header-link'>bogosorter</div>
+                </a>
+            }
+            <div className='header-space'></div>
+            <a href="https://github.com/bogosorter/vortex" data-rr-ui-event-key="https://github.com/bogosorter" className="nav-link">
+                <div className='header-link'>
+                    <GitHub />
+                </div>
             </a>
         </div>
     )
